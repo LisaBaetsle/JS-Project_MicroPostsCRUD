c4eccac35d5e14d1169fd2438f9dc1185b3147c7
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-11-13 12:00:39 +0100

    add posts and show alert

--- src/ui.js
+++ src/ui.js
@@ -29,6 +29,34 @@ class UI {
 
     this.post.innerHTML = output;
   }
+
+  showAlert(message, className) {
+    this.clearAlert();
+
+    const div = document.createElement('div'); // Create div
+    div.className = className; // Add the classes
+    div.appendChild(document.createTextNode(message)); // Add the text as a child element
+    const container = document.querySelector(".postsContainer"); // Get parent
+    const posts = document.querySelector('#posts'); // Get posts
+    container.insertBefore(div, posts); // Insert the div before the posts in the parent element
+
+    // Time out
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
+  }
 }
 
 export const ui = new UI();
