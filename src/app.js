c5b7006dfb6ccff1f23483ccb3ea5fc7b6dd3387
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-11-13 11:42:33 +0100

    create UI module, create get Posts functionality

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+import { http } from './http';
+import { ui } from './ui';
+
+// Get posts on DOMload
+document.addEventListener('DOMContentLoaded', getPosts);
+
+function getPosts() {
+  http.get('http://localhost:3000/posts')
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
